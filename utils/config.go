ca7296fd9325aadf632c7426d6f56a867654402f
Aditya Bhalerao <adityab7968@gmail.com>
2024-12-04 00:06:47 +0530

    Add logger, config loader

--- utils/config.go
+++ utils/config.go
@@ -1,1 +1,24 @@
 package utils
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func LoadConfig(configPath string, config interface{}) error {
+	configFile, err := os.Open(configPath)
+	if err != nil {
+		Logger.Error().Err(err).Msg("[CONFIG-LOADER] failed to open config file")
+		return err
+	}
+	defer configFile.Close()
+
+	decoder := json.NewDecoder(configFile)
+	err = decoder.Decode(config)
+	if err != nil {
+		Logger.Error().Err(err).Msg("[CONFIG-LOADER] failed to decode config")
+		return err
+	}
+
+	return nil
+}
