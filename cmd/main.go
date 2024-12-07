3f1bbc43dd11f86926bbdcb9e79fe21e48440c6e
Aditya <adityab7968@gmail.com>
2024-12-07 23:03:39 +0530

    Add structs, partially implement main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/arbha1erao/load-balancer/lb"
+	"github.com/arbha1erao/load-balancer/utils"
+)
+
+type Config struct {
+	Servers             []lb.Server `json:"servers"`
+	HealthCheckInterval int         `json:"health_check_interval"`
+	RetryCount          int         `json:"retry_count"`
+	WebhookAlertURL     string      `json:"webhook_alert_url"`
+}
+
+func main() {
+	utils.NewLogger()
+	utils.Logger.Info().Msg("[MAIN] load balancer starting")
+
+	var config Config
+
+	if err := utils.LoadConfig("config.json", &config); err != nil {
+		utils.Logger.Fatal().Err(err).Msg("[MAIN] failed to load configuration")
+	}
+
+	utils.Logger.Info().Msg("[MAIN] load balancer started")
+}
--- lb/load_balancer.go
+++ lb/load_balancer.go
@@ -1,1 +1,17 @@
 package lb
+
+import "sync"
+
+type Server struct {
+	URL         string
+	Weight      int
+	Active      *bool
+	Connections *int
+}
+
+type LoadBalancer struct {
+	Servers  []*Server
+	mutex    sync.Mutex
+	rrIndex  int
+	Strategy string
+}
