ed6ba95e45f5dbfae59c7e6cd85d1c0672f6050e
Aditya <adityab7968@gmail.com>
2024-12-09 23:55:31 +0530

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/arbha1erao/load-balancer/health"
 	"github.com/arbha1erao/load-balancer/lb"
+	"github.com/arbha1erao/load-balancer/routing"
 	"github.com/arbha1erao/load-balancer/utils"
 )
 
@@ -22,5 +27,24 @@ func main() {
 		utils.Logger.Fatal().Err(err).Msg("[MAIN] failed to load configuration")
 	}
 
+	loadBalancer := &lb.LoadBalancer{Servers: make([]*lb.Server, len(config.Servers))}
+	for i, s := range config.Servers {
+		loadBalancer.Servers[i] = &lb.Server{URL: s.URL, Weight: s.Weight, Active: true}
+	}
+
+	go health.HealthCheck(loadBalancer.Servers, config.HealthCheckInterval, config.WebhookAlertURL)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := routing.ForwardRequest(loadBalancer, r, config.RetryCount)
+		if err != nil {
+			http.Error(w, "Service unavailable", http.StatusServiceUnavailable)
+			return
+		}
+		defer resp.Body.Close()
+
+		w.WriteHeader(resp.StatusCode)
+	})
+
 	utils.Logger.Info().Msg("[MAIN] load balancer started")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/arbha1erao/load-balancer
 
 go 1.23.0
 
-require github.com/rs/zerolog v1.33.0
+require (
+	github.com/rs/zerolog v1.33.0
+	golang.org/x/exp v0.0.0-20241204233417-43b7b7cde48d
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
